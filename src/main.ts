ce73fa321f51d05f5e06ae3b7f4b7319cbe3bb58
CarlosMejiaUTM <mejicarlos3210@gmail.com>
2025-07-13 11:06:21 -0600

    Cambiando credenciales a base64 para que render lo pueda leer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+
+function createGoogleCredentialsFile() {
+  const base64 = process.env.GCP_CREDENTIALS_BASE64;
+  if (!base64) {
+    throw new Error('La variable de entorno GCP_CREDENTIALS_BASE64 no está definida');
+  }
+  const filePath = path.join(process.cwd(), 'gcp-credentials.json');
+  const buffer = Buffer.from(base64, 'base64');
+  fs.writeFileSync(filePath, buffer);
+
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = filePath;
+}
 
 async function bootstrap() {
+  // Primero creamos el archivo de credenciales antes de arrancar la app
+  createGoogleCredentialsFile();
+
   const app = await NestFactory.create(AppModule);
 
   // Habilitamos CORS para que tu frontend pueda hacer peticiones
